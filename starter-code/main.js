81acea2a5fa3602ecd13317fa04df3d45a63163e
Lauren Templeton <etothel0514@gmail.com>
2016-09-18 23:10:37 -0400

    unit 11 added images and simulated flipping of cards

--- starter-code/main.js
+++ starter-code/main.js
@@ -11,17 +11,65 @@ else {
 }*/
 
 
-var numberOfCards = 4;
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
 
 
-function createBoard(numberOfCards){
+function createBoard(){
 	var gameBoard = document.getElementById('game-board');
 
-	for (var i = 0; i < numberOfCards; i++) {
-		var card = document.createElement('div');
-		card.className = 'card';
-		gameBoard.appendChild(card);
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('div');
+		cardElement.className = 'card';
+		cardElement.setAttribute('data-card', cards[i]);
+		// when a card is clicked the function isTwoCards will be executed
+		cardElement.addEventListener('click', isTwoCards)
+
+
+		gameBoard.appendChild(cardElement);
 	}
 }
 
-createBoard(numberOfCards);
+
+
+function isMatch(cardsInPlay){
+	var isMatch = cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1];
+	
+	return isMatch;
+}
+
+//checks to see if there are cards in play
+function isTwoCards() {
+
+  // add card to array of cards in play
+  // 'this' hasn't been covered in this prework, but
+  // for now, just know it gives you access to the card the user clicked on
+  var cardType = this.getAttribute('data-card');
+  cardsInPlay.push(cardType);
+
+  //show card image
+  this.innerHTML = '<img src="' + cardType + '.png" alt="' + cardType +'" />';
+
+
+  // if you have two cards in play check for a match
+  if (cardsInPlay.length === 2) {
+
+    // pass the cardsInPlay as an argument to isMatch function
+    isMatch(cardsInPlay);
+	
+
+    // clear cards visually
+    var allCards = document.getElementsByClassName('card');
+    	for (var i = 0; i < allCards.length; i++) {
+    		allCards[i].innerHTML = '';
+    	}
+
+    // clear cards in play array for next try
+    cardsInPlay = [];
+
+  }
+
+}
+
+
+createBoard();
