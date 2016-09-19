b29275279b6cfa509a849d242c79b6de79dbf1f6
Lauren Templeton <etothel0514@gmail.com>
2016-09-18 21:48:53 -0400

    unit 10 assignment cards are now dynamic

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,9 +3,25 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
-if (cardOne === cardTwo || cardThree === cardFour){
+/*if (cardOne === cardTwo || cardThree === cardFour){
 	alert("You found a match!");
 }
 else {
 	alert("Sorry, try again.");
+}*/
+
+
+var numberOfCards = 4;
+
+
+function createBoard(numberOfCards){
+	var gameBoard = document.getElementById('game-board');
+
+	for (var i = 0; i < numberOfCards; i++) {
+		var card = document.createElement('div');
+		card.className = 'card';
+		gameBoard.appendChild(card);
+	}
 }
+
+createBoard(numberOfCards);
